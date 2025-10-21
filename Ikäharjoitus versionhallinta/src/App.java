4d5789f154620b06cc4a9e19fb172ee2754374b4
Robert <robertvhtr@gmail.com>
2025-10-22 00:40:42 +0300

    Auto sekä kevarioikeudet lisätty

--- Ikäharjoitus versionhallinta/src/App.java
+++ Ikäharjoitus versionhallinta/src/App.java
@@ -1,14 +1,24 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-int ika = 15;
+int ika = 18;
 
 // Tulostusehdot
+
+//alaikäiset
 if (ika > 0 && ika < 18) {
     System.out.println("Olet alaikäinen");
     if (ika >= 15) {
     System.out.println("Saat ajaa mopoa");
-
+    }
+    if (ika >= 16 && ika <= 17) {
+    System.out.println("Saat ajaa kevaria");
+}
+}
+// täysi-ikäiset
+else if (ika == 18) {
+System.out.println("Olet juuri tullut täysi-ikäiseksi");
+System.out.println("Saat ajaa autoa");
 }
 else if (ika >= 65) {
     System.out.println("Olet eläkeläinen");
@@ -21,4 +31,4 @@ else {
 
     }
 }
-}
+
